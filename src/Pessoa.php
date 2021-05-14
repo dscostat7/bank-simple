f51e3149a60a227f60226017dea7baa0baa8c64c
Diego_Souza <diego.s.costa@fiesc.com.br>
2021-05-14 19:48:11 -0300

    Criando a classe Pessoa e Funcionario. Ajustes de codigo para se adequar a realidade.

---
+++ src/Pessoa.php
@@ -0,0 +1,33 @@
+<?php
+
+class Pessoa
+{
+    protected $nome;
+    protected $cpf;
+
+    public function __construct(string $nome, Cpf $cpf)
+    {
+        $this->validaNomeTitular($nome);
+        $this->nome = $nome;
+        $this->cpf = $cpf;
+    }
+
+
+    public function recuperaNome() : string 
+    {
+        return $this->nome;
+    }
+
+    public function recuperaCpf() : string
+    {
+        return $this->cpf->recuperaNumero();
+    }
+
+    protected function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 3) {
+            echo "O Campo Nome precisa ter pelo menos 3 caracteres" . PHP_EOL;
+            exit();
+        } 
+    }
+}
--- src/Titular.php
+++ src/Titular.php
@@ -1,36 +1,14 @@
 <?php
 
-class Titular 
+class Titular extends Pessoa
 {
-    private $cpf;
-    private $nome;
     private $endereco;
 
 
     public function __construct(Cpf $cpf, string $nome, Endereco $endereco)
     {
-        $this->cpf = $cpf;
-        $this->nome = $nome;
-        $this->validaNomeTitular($nome);
+        parent::__construct($nome, $cpf);
         $this->endereco = $endereco;
-    }
-
-    public function recuperaCpf() : string
-    {
-        return $this->cpf->recuperaNumero();
-    }
-
-    public function recuperaNome() : string 
-    {
-        return $this->nome;
-    }
-
-    private function validaNomeTitular(string $nomeTitular)
-    {
-        if (strlen($nomeTitular) < 3) {
-            echo "O Campo Nome precisa ter pelo menos 3 caracteres" . PHP_EOL;
-            exit();
-        } 
     }
 
     public function recuperaEndereco() : Endereco
